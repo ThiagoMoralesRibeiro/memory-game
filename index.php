0c2144a6375bcebc95715616a9c35903aaa1ab42
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2023-09-30 21:27:43 -0300

    Resolving the problem with cols and rows

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/main.css">
+    <!-- <link rel="stylesheet" href="./css/main.css"> -->
     <title>Memory-game</title>
 </head>
 <body>
+    <?php
+    if (!isset($_POST['send'])) {
+    ?>
     <section class="center-container">
-        <?php
-        if (!isset($_POST['send'])) {
-        ?>
         <form action="" method="post">
             <p>Colunas:</p><input type="number" name="number_cols" id="">
             <p>Linhas:</p><input type="number" name="number_rows" id="">
@@ -18,42 +18,49 @@
         </form>
     </section>
     <?php
-        } else {
-            $cols = isset($_REQUEST['number_cols']) ? $_REQUEST['number_cols'] : 4;
-            $rows = isset($_REQUEST['number_rows']) ? $_REQUEST['number_rows'] : 4;
-    
-            //Necessito sempre ter uma réplica da minha imagem
-    
-            //echo 'Colunas: '. $cols;
-            //echo ' Linhas: '. $rows;
-            $numberOfCards = $cols * $rows;
-            $numberOfImages = $numberOfCards / 2;
-            //echo "<br>".$numberOfCards. "<br>";
-            $maxNumberOfImages = 5;
-            $images = array();
+    } else {
+        $cols = isset($_REQUEST['number_cols']) ? $_REQUEST['number_cols'] : 4;
+        $rows = isset($_REQUEST['number_rows']) ? $_REQUEST['number_rows'] : 4;
 
-            //echo $numberOfImages;
-        
-            for ($i = 1; $i <= $maxNumberOfImages; $i++) { 
+        //Necessito sempre ter uma réplica da minha imagem
+
+        //echo 'Colunas: '. $cols;
+        //echo ' Linhas: '. $rows;
+        $numberOfCards = $cols * $rows;
+        $numberOfImages = $numberOfCards / 2;
+        //echo "<br>".$numberOfCards. "<br>";
+        $maxNumberOfImages = 5;
+        $images = array();
+
+        //echo $numberOfImages;
+        for ($i = 1; $i <= $maxNumberOfImages; $i++) { 
+            array_push($images, $i);
+            if (in_array($i, $images)) {
                 array_push($images, $i);
-                if (in_array($i, $images)) {
-                    array_push($images, $i);
-                }
-                echo $images[$i]. "<br>";
             }
-            shuffle($images);
-            if (!empty($images)) {
-                echo "<section class='memory-game'>";
-                for ($i = 0; $i < $numberOfCards; $i++) { 
-                    echo "<div class='memory-card'>";
-                    if (isset($images[$i])) {
-                        echo '<div><img src="img/'.$images[$i].'.png"/></div>';
-                    }
-                    
-                }
-            }
+            //echo $images[$i]. "<br>";
         }
-    ?>
+        shuffle($images);
+
+        if (!empty($images)) {
+            echo "<section class='memory-game'>";
+            for ($rowIndex=0; $rowIndex < $rows; $rowIndex++) { 
+                echo "<div class='memory-row'>";
+                for ($colIndex=0; $colIndex < $cols; $colIndex++) { 
+                    $takeCard = array_shift($images); 
+                    if (isset($images)) {
+                        echo '<img class="up-card" src="img/'.$takeCard.'.png" alt="">';
+                        echo '<img class="down-card" src="./img/back-card.png" "img/></div>';
+                    }
     
+                }
+                echo "</div>";
+            }
+            echo "</section>";
+            echo "</div>";
+        }
+    }
+    ?>
+
 </body>
 </html>
